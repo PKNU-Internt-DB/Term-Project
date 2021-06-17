03b4b8465ef7f9e05470f1c662e40e0ed8dd639a
litmuss <sunsu2737@gmail.com>
2021-06-17 22:00:08 +0900

    파일구조예시

---
+++ inventory/mainPage.py
@@ -0,0 +1,2 @@
+def getMain():
+    pass
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .mainPage import getMain
+from .productpage import getProduct
+@csrf_exempt
+def mainPage(request):
+    if request.method == 'GET':
+        return getMain()
 
-# Create your views here.
+@csrf_exempt
+def productPage(request):
+    if request.method=='GET':
+        return getProduct()
